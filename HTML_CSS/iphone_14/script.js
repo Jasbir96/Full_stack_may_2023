df69b0c39aec2150f83e3092e833d824459cf935
Jasbir <mr.jasbirsingh96@gmail.com>
2023-06-16 23:30:02 +0530

    updated the code with crousel

--- HTML_CSS/iphone_14/script.js
+++ HTML_CSS/iphone_14/script.js
@@ -1,39 +1,67 @@
-// how do you know that i want to select a color : -> click
-// give a color -> add a class
-
-console.log("Hello");
-
-// sure -> in html is loaded 
-
-// window ->inbuilt obj that reperesents the browser window
-// listen -> load even -> html , css and all the assets are loaded
+// load event is fired when html and all the external resources -> images , styles are loaded
 window.addEventListener("load", init);
 
-
 function init() {
-    // i am sure that page is loaded;
-    // html -> document
+    // console.log("Hello");
+    const colors = ["midnight", "starlight", "red", "blue", "yellow"];
     const heroNavContainer = document.querySelector(".hero_nav--container");
-    const colorPickerArr = document.querySelectorAll(".color_picker");;
+    const colorPickerArray = document.querySelectorAll(".color_picker");
+    const crouselIphoneArray = document.querySelectorAll(".crousel_iphone");
+    const heroCrouselContainer = document.querySelector(".hero_crousel_container");
+    let prevColor = "";
+    heroNavContainer.addEventListener("click", handlecolorUpdate);
 
-    heroNavContainer.addEventListener("click", handleColorUpdate);
 
-    function handleColorUpdate(eventObj) {
-        // console.log("elem",eventObj.target);
-        let currentColorElem = eventObj.target;
-        if (!currentColorElem.classList.contains("color_picker"))
-            return;
+    function handlecolorUpdate(e) {
 
-        // remove from  all pickers
-        for (let i = 0; i < colorPickerArr.length; i++) {
-            let alltheclasses = colorPickerArr[i].classList;
-            alltheclasses.remove("selected--color");
+        console.log("e",e,e.target)
+        const currentColorOption = e.target;
+
+        if (!e.target.classList.contains("color_picker")) {
+            return
+        }
+        addBorderToColorPickers(currentColorOption, colorPickerArray);
+        selectIphone(currentColorOption, crouselIphoneArray);
+    }
+
+
+    function selectIphone(currentColorOption, crouselIphoneArray) {
+        const currentColor = currentColorOption.classList[1];
+        console.log("currentColor", currentColor);
+        // ****************scaling and translating of individual iphones***/
+        for (let i = 0; i < crouselIphoneArray.length; i++) {
+            crouselIphoneArray[i].classList.remove("selected--iphone");
         }
 
-        // add to my target elem
-        currentColorElem.classList.add("selected--color");
+        for (let i = 0; i < crouselIphoneArray.length; i++) {
+            const cIphoneColor = crouselIphoneArray[i].classList[1];
+            if (cIphoneColor == currentColor) {
+                crouselIphoneArray[i].classList.add("selected--iphone");
+            }
+        }
+
+        //***** move background*****/ 
+        if (heroCrouselContainer.classList.length == 1) {
+            heroCrouselContainer.classList.add(currentColor);
+            prevColor = currentColor;
+        } else {
+            heroCrouselContainer.classList.replace(prevColor, currentColor);
+            prevColor = currentColor;
+        }
 
 
+
+    }
+
+
+
+    function addBorderToColorPickers(currentColorOption, colorPickerArray) {
+        //    console.log( currentColorOption.classList);
+
+        for (let i = 0; i < colorPickerArray.length; i++) {
+            colorPickerArray[i].classList.remove("selected--color");
+        }
+        currentColorOption.classList.add("selected--color");
     }
 
 }
