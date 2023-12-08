59ac1c9f324bcfe8763ef490a7ff20595d84b087
Jasbir <mr.jasbirsingh96@gmail.com>
2023-12-08 21:50:16 +0530

    updated example of payment

--- Projects_Module/code/Lecture_10_reviews/1_Payment_POC/backend/api.js
+++ Projects_Module/code/Lecture_10_reviews/1_Payment_POC/backend/api.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const Razorpay = require("razorpay");
 const shortId = require("shortid");
 const cors = require("cors");
+// it is part of nodejs 
+const crypto = require("crypto");
 
 const app = express();
 dotenv.config();
@@ -18,16 +20,17 @@ const razorpayInstance = new Razorpay({
     key_secret: PRIVATE_KEY,
 });
 
-
+// initial booking
 app.post("/checkout", async function (req, res) {
     const amount = 500;
     const currency = "INR";
+    const payment_capture = 1;
     try {
-
         const options = {
             amount: amount * 100,
             currency: currency,
             receipt: shortId.generate(),
+            payment_capture: payment_capture
         }
         // you create an order -> you are telling your payment gateway that some user 
         // is going to pay in a bit  of time 
@@ -50,11 +53,27 @@ app.post("/checkout", async function (req, res) {
 
 
 })
-
-app.post("/verification", async function (req,res) {
+// payment verification
+app.post("/verification", async function (req, res) {
     try {
-        console.log("hi", req.body);
-        res.json({ status: "ok" });
+        // this object -> sha256+webhook_secret
+        const shasum = crypto.createHmac("sha256", WEBHOOK_SECERET);
+        shasum.update(JSON.stringify(req.body));
+        const freshSignature = shasum.digest("hex");
+        const razorPaySign = req.headers["x-razorpay-signature"];
+        // console.log(req.headers);;
+        console.log(freshSignature, razorPaySign);
+        if (freshSignature == razorPaySign) {
+            // ok
+            console.log("Payment is verified", req.body);
+            res.status(200).json({
+                message: "OK",
+            });
+        } else {
+            // there some tempering 
+            res.status(403).json({ message: "Invalid" });
+        }
+
     } catch (err) {
         res.status(500).json({
             status: "failure",
@@ -62,8 +81,6 @@ app.post("/verification", async function (req,res) {
         })
     }
 })
-
-
 
 app.listen(3000, function () {
     console.log("Listening at port 3000");
